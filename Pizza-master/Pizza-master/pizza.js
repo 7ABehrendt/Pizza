36105c2c261e0529a4cbde386b11d869e806cf42
7ABehrendt <7abehrendt@stpatrick.org>
2016-11-17 10:53:04 -0600

    started making a pizza constructor

---
+++ Pizza-master/Pizza-master/pizza.js
@@ -0,0 +1,31 @@
+class pizza{
+  //type constructor below
+  constructor(s,m,v){
+    this.size = s
+    this.meatToppings = m
+    this.veggieToppings = v
+    }
+  //type instance functions below
+  sizeCost(){
+    if(s=="Small"){
+      price = 7.99
+    }
+    else if (s=="Medium") {
+      price = 9.99
+    }
+    else if (s=="Large") {
+      price = 12.99
+    }
+    else (s=="Extra-Large") {
+      price = 15.99
+    }
+  }
+  this.toppingCost={
+    if(topping)
+  }
+
+
+  //type class functions below
+
+
+}
