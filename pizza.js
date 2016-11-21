dd26bf76d058fc530d748596298136f58ca553a9
7ABehrendt <7abehrendt@stpatrick.org>
2016-11-21 11:20:07 -0600

    finished da pizza

---
+++ pizza.js
@@ -0,0 +1,46 @@
+class pizza{
+  //type constructor below
+  constructor(s,m,v){
+    this.size = s
+    this.meatToppings = m
+    this.veggieToppings = v
+    }
+  //type instance functions below
+  this.sizeCost = function(){
+    if(this.size=="Small"){
+      return 7.99
+    }
+    else if (this.size=="Medium") {
+      return 9.99
+    }
+    else if (this.size=="Large") {
+      return 12.99
+    }
+    else (this.size=="Extra-Large") {
+      return 15.99
+    }
+  }
+  this.toppingCost= function(topping){
+    if(topping=="meat"){
+      return(this.meatToppings.length *.99)
+    }
+    else if(topping=="veggie"){
+      return (this.veggieToppings.length *.50)
+    }
+  }
+  this.price = function(){
+    return (this.sizeCost()+this.toppingCost(meat+veggie))
+  }
+
+
+
+}
+
+
+  //type class functions below
+static promotionalDeal(pizza,percent){
+  let percent = 1 - (percent/100)
+  return this.price()*percent
+}
+
+© 2016 GitHub, Inc. Terms Privacy Sec
